ed83b3fe61ddf396d0633ce7efab82799ba02856
maxcaplan <maxacaplan@gmail.com>
2026-04-06 20:04:08 -0300

    Added web fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,87 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Fira Code",
+      cssVariable: "--type--font--display",
+      fallbacks: ["monospace"],
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/fira-code/FiraCode-Regular.woff2"],
+            style: "normal",
+            weight: 400,
+          },
+          {
+            src: ["./src/assets/fonts/fira-code/FiraCode-Medium.woff2"],
+            style: "normal",
+            weight: 500,
+          },
+          {
+            src: ["./src/assets/fonts/fira-code/FiraCode-SemiBold.woff2"],
+            style: "normal",
+            weight: 600,
+          },
+          {
+            src: ["./src/assets/fonts/fira-code/FiraCode-Bold.woff2"],
+            style: "normal",
+            weight: 700,
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.local(),
+      name: "Fira Sans",
+      cssVariable: "--type--font--body",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/fira-sans/FiraSans-Regular.woff2"],
+            style: "normal",
+            weight: 400,
+          },
+          {
+            src: ["./src/assets/fonts/fira-sans/FiraSans-Medium.woff2"],
+            style: "normal",
+            weight: 500,
+          },
+          {
+            src: ["./src/assets/fonts/fira-sans/FiraSans-SemiBold.woff2"],
+            style: "normal",
+            weight: 600,
+          },
+          {
+            src: ["./src/assets/fonts/fira-sans/FiraSans-Bold.woff2"],
+            style: "normal",
+            weight: 700,
+          },
+          {
+            src: ["./src/assets/fonts/fira-sans/FiraSans-Italic.woff2"],
+            style: "italic",
+            weight: 400,
+          },
+          {
+            src: ["./src/assets/fonts/fira-sans/FiraSans-MediumItalic.woff2"],
+            style: "italic",
+            weight: 500,
+          },
+          {
+            src: ["./src/assets/fonts/fira-sans/FiraSans-SemiBoldItalic.woff2"],
+            style: "italic",
+            weight: 600,
+          },
+          {
+            src: ["./src/assets/fonts/fira-sans/FiraSans-BoldItalic.woff2"],
+            style: "italic",
+            weight: 700,
+          },
+        ],
+      },
+    },
+  ],
+});
