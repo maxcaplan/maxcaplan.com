1831422c3543fba89ff80bf1162a8e8845d8fff0
maxcaplan <maxacaplan@gmail.com>
2026-04-10 16:33:31 -0300

    Refactored icon generation for maxcaplan-icons

---
+++ packages/maxcaplan-icons/src/components/Icon.tsx
@@ -0,0 +1,53 @@
+import React, { useMemo } from "react";
+
+export interface IconProps<
+  Styles extends string | number | symbol,
+> extends Omit<React.ComponentPropsWithRef<"svg">, "children"> {
+  ["icon-style"]?: Styles;
+}
+
+export type Icon<Styles extends string | number | symbol> = (
+  props?: IconProps<Styles>,
+) => React.JSX.Element;
+
+export interface IconStyleData {
+  root: (props?: React.ComponentPropsWithRef<"svg">) => React.JSX.Element;
+  content: React.ReactNode;
+}
+
+/** Create a new icon component */
+export function createIconComponent<Styles extends string | number | symbol>(
+  key: string,
+  styles: Record<Styles, IconStyleData>,
+): Icon<Styles> {
+  if (Object.values(styles).length <= 0) {
+    throw new Error(`Cannot create icon ${key} component with no styles`);
+  }
+
+  return function Icon(props) {
+    const {
+      "icon-style": icon_style,
+      className,
+      ...element_props
+    } = props ?? {};
+
+    const IconStyle = useMemo(
+      () =>
+        icon_style !== undefined && styles[icon_style] !== undefined
+          ? styles[icon_style]
+          : Object.values<IconStyleData>(styles)[0], // Default style
+      [icon_style],
+    );
+
+    return (
+      <IconStyle.root
+        className={[`mc-icon mc-icon--${key}`, className]
+          .filter((v) => !!v)
+          .join(" ")}
+        {...element_props}
+      >
+        {IconStyle.content}
+      </IconStyle.root>
+    );
+  };
+}
