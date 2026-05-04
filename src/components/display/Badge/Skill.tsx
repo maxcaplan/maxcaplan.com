44ae76ccdb29fd7c2a60d413ba701c5f005f0273
maxcaplan <maxacaplan@gmail.com>
2026-05-04 12:03:45 -0300

    Added conditional label visual rendering for skill badge component

--- src/components/display/Badge/Skill.tsx
+++ src/components/display/Badge/Skill.tsx
@@ -3,6 +3,7 @@ import Badge, { type BadgeColour } from ".";
 
 interface SkillBadgeProps {
   skill: CollectionEntry<"skills">;
+  label?: boolean;
 }
 
 type SkillColour = Required<CollectionEntry<"skills">["data"]>["colour"];
@@ -17,15 +18,26 @@ const badge_colour_map: BadgeColourMap = {
 
 /** A small label for displaying a skill */
 export default function SkillBadge(props: SkillBadgeProps) {
+  const variant =
+    props.label === false
+      ? "icon"
+      : props.skill.data.icon
+        ? "icon-left"
+        : undefined;
+
   return (
     <Badge
       icon={props.skill.data.icon}
-      variant={props.skill.data.icon && "icon-left"}
+      variant={variant}
       colour={
         props.skill.data.colour && badge_colour_map[props.skill.data.colour]
       }
     >
-      {props.skill.id}
+      {variant === "icon" && (
+        <span class="visually-hidden">{props.skill.id}</span>
+      )}
+
+      {variant !== "icon" && <>{props.skill.id}</>}
     </Badge>
   );
 }
