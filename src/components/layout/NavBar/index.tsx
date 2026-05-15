78610c8ba4f28f43d0b415894dea517a5bbb5749
maxcaplan <maxacaplan@gmail.com>
2026-05-15 10:07:02 -0300

    Added a navbar layout component

---
+++ src/components/layout/NavBar/index.tsx
@@ -0,0 +1,20 @@
+import "./styles.scss";
+
+import clsx from "clsx";
+import type { HTMLAttributes } from "preact";
+
+export interface NavBarProps extends HTMLAttributes<HTMLDivElement> {}
+
+/** A container for navigation typically found at the top of the page */
+export default function NavBar(props: NavBarProps) {
+  const { class: class_attribute, className, children, ...attributes } = props;
+
+  return (
+    <div
+      {...attributes}
+      class={clsx("navbar", "root-padding", class_attribute, className)}
+    >
+      <div class="navbar__inner container-full">{children}</div>
+    </div>
+  );
+}
